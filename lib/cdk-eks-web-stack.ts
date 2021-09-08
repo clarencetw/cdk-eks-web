364505ad3a114e876b2e25879427274ec067a941
Clarence <me@clarence.tw>
2021-09-09 01:06:30 +0800

    Add eks cluster

--- lib/cdk-eks-web-stack.ts
+++ lib/cdk-eks-web-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import * as eks from '@aws-cdk/aws-eks';
+import * as iam from '@aws-cdk/aws-iam';
 
 export class CdkEksWebStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const cluster = new eks.Cluster(this, 'eks', {
+      version: eks.KubernetesVersion.V1_21,
+    });
+    const adminUser = iam.User.fromUserName(this, 'adminUser', 'clarence');
+    cluster.awsAuth.addUserMapping(adminUser, { groups: [ 'system:masters' ]});
   }
 }
