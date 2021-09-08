1249cf4b0dbc992f0084ed8fca95bf4802f1d96d
Clarence <me@clarence.tw>
2021-09-09 02:55:50 +0800

    Add spot ec2

--- lib/cdk-eks-web-stack.ts
+++ lib/cdk-eks-web-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as eks from '@aws-cdk/aws-eks';
 import * as iam from '@aws-cdk/aws-iam';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class CdkEksWebStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,5 +12,14 @@ export class CdkEksWebStack extends cdk.Stack {
     });
     const adminUser = iam.User.fromUserName(this, 'adminUser', 'clarence');
     cluster.awsAuth.addUserMapping(adminUser, { groups: [ 'system:masters' ]});
+
+    cluster.addNodegroupCapacity('spot', {
+      instanceTypes: [
+        new ec2.InstanceType('c5.large'),
+        new ec2.InstanceType('c5a.large'),
+        new ec2.InstanceType('c5d.large'),
+      ],
+      capacityType: eks.CapacityType.SPOT,
+    });
   }
 }
