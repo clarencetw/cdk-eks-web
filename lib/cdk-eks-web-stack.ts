52a9e495802fa81ec6701c6dc2fa277ab1c8199b
Clarence <me@clarence.tw>
2021-09-12 22:48:55 +0800

    Add nvidia ec2

--- lib/cdk-eks-web-stack.ts
+++ lib/cdk-eks-web-stack.ts
@@ -2,6 +2,9 @@ import * as cdk from '@aws-cdk/core';
 import * as eks from '@aws-cdk/aws-eks';
 import * as iam from '@aws-cdk/aws-iam';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as fs from 'fs';
+import * as YAML from 'yaml';
+import * as path from 'path';
 
 export class CdkEksWebStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -14,18 +17,14 @@ export class CdkEksWebStack extends cdk.Stack {
     const adminUser = iam.User.fromUserName(this, 'adminUser', 'clarence');
     cluster.awsAuth.addUserMapping(adminUser, { groups: ['system:masters'] });
 
-    cluster.addNodegroupCapacity('spot', {
-      instanceTypes: [
-        new ec2.InstanceType('c5.large'),
-        new ec2.InstanceType('c5a.large'),
-        new ec2.InstanceType('c5d.large'),
-      ],
+    cluster.addNodegroupCapacity('gpu', {
+      instanceTypes: [new ec2.InstanceType('p2.xlarge')],
       capacityType: eks.CapacityType.SPOT,
-      taints: [{
-        effect: eks.TaintEffect.NO_SCHEDULE,
-        key: 'type',
-        value: 'spot',
-      }],
+      amiType: eks.NodegroupAmiType.AL2_X86_64_GPU,
+      diskSize: 100,
     });
+    const nvidiaDevicePlugin = fs.readFileSync(path.join(__dirname, '../lib', 'addons/nvidia-device-plugin.yml'), 'utf8');
+    const nvidiaManifests = YAML.parse(nvidiaDevicePlugin);
+    cluster.addManifest(`nvidia-device-plugin`, nvidiaManifests);
   }
 }
